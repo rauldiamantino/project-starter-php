0f76d554a3abe37c82af95ee6927332509ba9fc6
Raul Diamantino <rauldiamantino25@gmail.com>
2025-06-08 02:20:51 -0300

    Fix: Do not import Throwable interface in index

--- Public/index.php
+++ Public/index.php
@@ -2,7 +2,6 @@
 
 require_once '../vendor/autoload.php';
 
-use Throwable;
 use App\Exceptions\CompanyNotExistsException;
 use Core\Dbal\Exceptions\EntityNotFoundException;
 
@@ -15,7 +14,7 @@ try {
     http_response_code(404);
     header('Location: /404');
     exit;
-} catch (Throwable $e) {
+} catch (\Throwable $e) {
     http_response_code(500);
     error_log($e->getMessage());
     header('Location: /500');
